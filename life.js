8fa98d75f49133ff3f27873af04ef11c30fe0589
Thompson Smooth OKOYEN <105185847+MicroSmooth@users.noreply.github.com>
2022-06-22 23:09:21 +0100

    Add files via upload

---
+++ life.js
@@ -0,0 +1,17 @@
+alert('I\'m Thompson Smooth, 1.64 metres in Height from Nigeria')
+
+let name = 'thompson smooth'
+let height = '1.64 metres'
+let country = 'Nigeria'
+
+
+let nameCap = name.toUpperCase();
+let y = name.slice(1, 9);
+let z = name.slice(10, 16);
+let firstLetter = name[0].toUpperCase();
+let secondLetter = name[9].toUpperCase();
+let result = firstLetter+y+secondLetter+z;
+
+console.log(result)
+console.log(height)
+console.log(country)
